e9f80c1052d82856ca66fd8b6eed8ef47549076c
Seqian Wang <seqian@outlook.com>
2015-01-29 16:45:28 -0500

    determineDF in RFT
    New script that automatically adjust degrees of freedom based on number
    of subjects for each group

--- a9rftImages.sh
+++ a9rftImages.sh
@@ -1,17 +1,26 @@
-
 function determineDF () {
-	df
+	df=0
 	case "$i"
-		*mcsa*)
-	
-		;;
 		*adni*)
-		
+		case "$i"
+			*1*); df=`expr $df + 60`;;
+			*2*); df=`expr $df + 53`;;
+			*3*); df=`expr $df + 40`;;
+			*4*); df=`expr $df + 37`;;
+		esac
+		;;
+		*mcsa*)
+			case "$i"
+			*1*); df=`expr $df + 17`;;
+			*3*); df=`expr $df + 12`;;
+			*4*); df=`expr $df + 9`;;
 		;;
 	esac
+	df=`expr $df - 2`;
 }
 
 for i in `ls /data/data03/wang/output2/*.mnc`; do
 	determineDF
-	runRFT.sh <file> <smoothing> <degrees of freedom>
+	echo $i $df
+	#runRFT.sh $i 6 "$df"
 done
