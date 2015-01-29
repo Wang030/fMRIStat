4c65e55bdb72cda1c2af89615a573928986e14e9
Seqian Wang <seqian@outlook.com>
2015-01-29 17:22:57 -0500

    a9rft working

--- a9rftImages.sh
+++ a9rftImages.sh
@@ -1,20 +1,22 @@
 function determineDF () {
 	df=0
 	if grep -q "adni" <<< "$i"; then
-		if grep -q "1" <<< "$i"; then df=`expr $df + 60`;;
-		if grep -q "2" <<< "$i"; then df=`expr $df + 53`;;
-		if grep -q "3" <<< "$i"; then df=`expr $df + 40`;;
-		if grep -q "4" <<< "$i"; then df=`expr $df + 37`;;
+		if grep -q "1" <<< "$i"; then df=`expr $df + 60`; fi
+		if grep -q "2" <<< "$i"; then df=`expr $df + 53`; fi
+		if grep -q "3" <<< "$i"; then df=`expr $df + 40`; fi
+		if grep -q "4" <<< "$i"; then df=`expr $df + 37`; fi
 	elif grep -q "mcsa" <<< "$i"; then
-		if grep -q "1" <<< "$i"; then df=`expr $df + 17`;;
-		if grep -q "3" <<< "$i"; then df=`expr $df + 12`;;
-		if grep -q "4" <<< "$i"; then df=`expr $df + 9`;;	
+		if grep -q "1" <<< "$i"; then df=`expr $df + 17`; fi
+		if grep -q "3" <<< "$i"; then df=`expr $df + 12`; fi
+		if grep -q "4" <<< "$i"; then df=`expr $df + 9`; fi
 	fi
 	df=`expr $df - 2`;
 }
 
-for i in `ls /data/data03/wang/output2/*.mnc`; do
+for h in `ls /data/data03/wang/output2/*.mnc`; do
+	i=`basename $h .mnc`
+	output=`dirname $h`/$i
+	mkdir -p "$output"
 	determineDF
-	echo $i $df
-	#runRFT.sh $i 6 "$df"
+	runRFT.sh $h 6 "$df" "$output"
 done
--- runRFT.sh
+++ runRFT.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then echo "Usage: $0 <file> <smoothing> <degrees of freedom>"; exit; fi
+if [ $# -ne 4 ]; then echo "Usage: $0 <file> <smoothing> <degrees of freedom> <output>"; exit; fi
 
 mask="/data/data03/wang/input/20140805_ADNI/fMRIStat/resampledMask.mnc"
 numberOfVoxels=`mincstats -mask $mask -mask_binvalue 1 -quiet -count $1`
-sh /home/wang/Documents/bin/fMRIStat/johnRft.sh -m /data/data03/wang/input/20140805_ADNI/fMRIStat/resampledMask.mnc -d $3 -s $2 --voxnumb $numberOfVoxels $1
+sh /home/wang/Documents/bin/fMRIStat/johnRft.sh -m /data/data03/wang/input/20140805_ADNI/fMRIStat/resampledMask.mnc -c -d $3 -s $2 --voxnumb $numberOfVoxels -o $4 $1
