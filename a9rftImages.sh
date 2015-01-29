8466e9bd260fb23aa70e5b653be60f7bc41ef02b
Seqian Wang <seqian@outlook.com>
2015-01-29 17:03:15 -0500

    Back to you

--- a9rftImages.sh
+++ a9rftImages.sh
@@ -1,19 +1,27 @@
 function determineDF () {
 	df=0
-	case "$i"
+	case "$i" in
 		*adni*)
-		case "$i"
-			*1*); df=`expr $df + 60`;;
-			*2*); df=`expr $df + 53`;;
-			*3*); df=`expr $df + 40`;;
-			*4*); df=`expr $df + 37`;;
+		case "$i" in
+			*1*)
+				df=`expr $df + 60`;;
+			*2*)
+				df=`expr $df + 53`;;
+			*3*)
+				df=`expr $df + 40`;;
+			*4*)
+				df=`expr $df + 37`;;
 		esac
 		;;
 		*mcsa*)
-			case "$i"
-			*1*); df=`expr $df + 17`;;
-			*3*); df=`expr $df + 12`;;
-			*4*); df=`expr $df + 9`;;
+		case "$i" in
+			*1*)
+				df=`expr $df + 17`;;
+			*3*)
+				df=`expr $df + 12`;;
+			*4*)
+				df=`expr $df + 9`;;
+		esac
 		;;
 	esac
 	df=`expr $df - 2`;
