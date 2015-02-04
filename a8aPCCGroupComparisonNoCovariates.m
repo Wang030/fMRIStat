c5a74179a65f1f6f3d6a3f9946facf90338b9df9
Seqian Wang <seqian@outlook.com>
2015-02-04 16:57:31 -0500

    Fixed exist check

--- a8aPCCGroupComparisonNoCovariates.m
+++ a8aPCCGroupComparisonNoCovariates.m
@@ -4,9 +4,9 @@ seed = '/PCC';
 % 1v4 adni
 clear data;
 data = readingXlsx('adni');
-outputBase = ['/data/data03/wang/output/20150204_PCC_adni/' seed '_1v4_adni_' strjoin(covariates{i},'_')];
+outputBase = ['/data/data03/wang/output/20150204_PCC_adni/' seed '_1v4_adni_noCovariates'];
 group1_files = replaceCellSubstring(data.cn.files,'/seed', seed);
 group2_files = replaceCellSubstring(data.ad.files,'/seed', seed);
-group1_covariates = {};
-group2_covariates = {};
+group1_covariates = [];
+group2_covariates = [];
 save([outputBase '.mat']);
