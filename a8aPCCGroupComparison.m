1c21cdd547478f31d87f0efa8b089fe22194234f
Seqian Wang <seqian@outlook.com>
2015-02-04 16:26:26 -0500

    Testing without any covariates

--- a8aPCCGroupComparison.m
+++ a8aPCCGroupComparison.m
@@ -6,9 +6,10 @@ covariates = {
     {'SITEID', 'apoe', 'PTEDUCAT', 'FD'}
     {'SITEID', 'apoe', 'PTGENDER', 'FD'}
     {'SITEID', 'apoe', 'scanAge', 'FD'}
-    {'SITEID', 'apoe', 'FD'}
+    {'SITEID', 'apoe', 'FD'
     };
 
+covariates = {''};
 % 1v4 adni
 for i = 1:length(covariates)
     clear data;
@@ -16,7 +17,7 @@ for i = 1:length(covariates)
     outputBase = ['/data/data03/wang/output/20150204_PCC_adni/' seed '_1v4_adni_' strjoin(covariates{i},'_')];
     group1_files = replaceCellSubstring(data.cn.files,'/seed', seed);
     group2_files = replaceCellSubstring(data.ad.files,'/seed', seed);
-    group1_covariates = data.cn.covariates;
-    group2_covariates = data.ad.covariates;
+    group1_covariates = {};
+    group2_covariates = {};
     save([outputBase '.mat']);
 end
