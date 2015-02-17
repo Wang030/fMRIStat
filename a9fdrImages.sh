a200bf4ea943a6208f8d3f77c8cecc8c5c87edf5
Seqian Wang <seqian@outlook.com>
2015-02-17 17:14:12 -0500

    Adapted df to new cohort list

--- a9fdrImages.sh
+++ a9fdrImages.sh
@@ -7,9 +7,9 @@ if [ -n "$2" ]; then mask="$2"; else mask="/data/data03/wang/input/20140805_ADNI
 function determineDF () {
 	df=0
 	if grep -q "adni" <<< "$i"; then
-		if grep -q "1" <<< "$i"; then df=`expr $df + 60`; fi
-		if grep -q "2" <<< "$i"; then df=`expr $df + 53`; fi
-		if grep -q "3" <<< "$i"; then df=`expr $df + 40`; fi
+		if grep -q "1" <<< "$i"; then df=`expr $df + 56`; fi
+		if grep -q "2" <<< "$i"; then df=`expr $df + 48`; fi
+		if grep -q "3" <<< "$i"; then df=`expr $df + 35`; fi
 		if grep -q "4" <<< "$i"; then df=`expr $df + 31`; fi
 	elif grep -q "mcsa" <<< "$i"; then
 		if grep -q "1" <<< "$i"; then df=`expr $df + 17`; fi
--- a9rftImages.sh
+++ a9rftImages.sh
@@ -2,12 +2,14 @@
 
 if [ $# -ne 1 -a $# -ne 2 ]; then echo "Usage: $0 <mincFile> (maskFile)"; exit; fi
 
+if [ -n "$2" ]; then mask="$2"; else mask="/data/data03/wang/input/20140805_ADNI/fMRIStat/resampledMask.mnc"; fi
+
 function determineDF () {
 	df=0
 	if grep -q "adni" <<< "$i"; then
-		if grep -q "1" <<< "$i"; then df=`expr $df + 60`; fi
-		if grep -q "2" <<< "$i"; then df=`expr $df + 53`; fi
-		if grep -q "3" <<< "$i"; then df=`expr $df + 40`; fi
+		if grep -q "1" <<< "$i"; then df=`expr $df + 56`; fi
+		if grep -q "2" <<< "$i"; then df=`expr $df + 48`; fi
+		if grep -q "3" <<< "$i"; then df=`expr $df + 35`; fi
 		if grep -q "4" <<< "$i"; then df=`expr $df + 31`; fi
 	elif grep -q "mcsa" <<< "$i"; then
 		if grep -q "1" <<< "$i"; then df=`expr $df + 17`; fi
