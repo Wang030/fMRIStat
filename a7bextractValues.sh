a90b9a51d0e6ec62ad9d27fcc8274e14b89d20c7
Seqian Wang <seqian@outlook.com>
2015-04-10 07:46:39 -0400

    Output as csv rather than txt

--- a7bextractValues.sh
+++ a7bextractValues.sh
@@ -8,12 +8,12 @@ if [ $# -lt 2 ]; then echo "Usage: $0 fileWithDesiredTests files"; exit; fi
 while [ -f "$1" ]; do
 	subj=`basename $1 | sed "s:subject\([0-9]\{3,4\}\).*:\1:"`
 	sess=`basename $1 | sed "s:.*session\([0-9]*\).*:\1:"`
-	echo -n "$subj $sess"
+	echo -n "$subj,$sess"
 	cat $testFile |
 	while read test; do
 		stat=`mincstats -quiet $test $1`
 		stat=`echo $stat | tr -d '\r'`
-		echo -n " $stat"
+		echo -n ",$stat"
 	done
 		echo ""
 	shift
