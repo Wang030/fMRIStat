b9a98d3f5d8fda5f8c984a9d1e38ba6455864400
Seqian Wang <seqian@outlook.com>
2015-06-23 20:24:10 -0400

    Added covariates consideration, -30 fwhm_varatio

--- a8aAdniGenerateMat.m
+++ a8aAdniGenerateMat.m
@@ -1,6 +1,6 @@
 % Input information
 data = readingXlsx('adni');
-outputFolder = '/data/data03/wang/output/20150220_fwhm30';
+outputFolder = '/data/data03/wang/output/20150623_groupComparisonHippocampusAtrophy';
 
 % Choose groups of interest
 
@@ -76,6 +76,54 @@ group1_covariates = data.cn.covariates;
 group2_covariates = data.emci.covariates;
 save([outputBase '.mat']);
 
+%% rA
+seed = 'rA';
+% 1v4 adni
+outputBase = [outputFolder '/gc_' seed '_1v4_adni'];
+group1_files = replaceCellSubstring(data.cn.files,'seed', seed);
+group2_files = replaceCellSubstring(data.ad.files,'seed', seed);
+group1_covariates = data.cn.covariates;
+group2_covariates = data.ad.covariates;
+save([outputBase '.mat']);
+% 1v3 adni
+outputBase = [outputFolder '/gc_' seed '_1v3_adni'];
+group1_files = replaceCellSubstring(data.cn.files,'seed', seed);
+group2_files = replaceCellSubstring(data.lmci.files,'seed', seed);
+group1_covariates = data.cn.covariates;
+group2_covariates = data.lmci.covariates;
+save([outputBase '.mat']);
+% 1v2 adni
+outputBase = [outputFolder '/gc_' seed '_1v2_adni'];
+group1_files = replaceCellSubstring(data.cn.files,'seed', seed);
+group2_files = replaceCellSubstring(data.emci.files,'seed', seed);
+group1_covariates = data.cn.covariates;
+group2_covariates = data.emci.covariates;
+save([outputBase '.mat']);
+
+%% rP
+seed = 'rP';
+% 1v4 adni
+outputBase = [outputFolder '/gc_' seed '_1v4_adni'];
+group1_files = replaceCellSubstring(data.cn.files,'seed', seed);
+group2_files = replaceCellSubstring(data.ad.files,'seed', seed);
+group1_covariates = data.cn.covariates;
+group2_covariates = data.ad.covariates;
+save([outputBase '.mat']);
+% 1v3 adni
+outputBase = [outputFolder '/gc_' seed '_1v3_adni'];
+group1_files = replaceCellSubstring(data.cn.files,'seed', seed);
+group2_files = replaceCellSubstring(data.lmci.files,'seed', seed);
+group1_covariates = data.cn.covariates;
+group2_covariates = data.lmci.covariates;
+save([outputBase '.mat']);
+% 1v2 adni
+outputBase = [outputFolder '/gc_' seed '_1v2_adni'];
+group1_files = replaceCellSubstring(data.cn.files,'seed', seed);
+group2_files = replaceCellSubstring(data.emci.files,'seed', seed);
+group1_covariates = data.cn.covariates;
+group2_covariates = data.emci.covariates;
+save([outputBase '.mat']);
+
 %% Anterior-Posterior
 % CN
 outputBase = [outputFolder '/xlAvP_1_adni'];
@@ -105,7 +153,7 @@ group2_files = replaceCellSubstring(data.ad.files,'seed', 'lP');
 group1_covariates = [];
 group2_covariates = [];
 save([outputBase '.mat']);
-%{
+
 %% Assymetry in controls
 % Anterior adni
 outputBase = [outputFolder '/zlvrA_1_adni'];
@@ -121,4 +169,3 @@ group2_files = replaceCellSubstring(data.cn.files,'seed', 'rP');
 group1_covariates = [];
 group2_covariates = [];
 save([outputBase '.mat']);
-%}
